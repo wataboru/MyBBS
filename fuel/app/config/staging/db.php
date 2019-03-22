4dd004d7b656534fe48779120ac2810d138b4455
Wataru Work <wataruwork@Boru-MBP.local>
2019-03-22 19:26:51 +0900

    staging/db.php updated.

--- fuel/app/config/staging/db.php
+++ fuel/app/config/staging/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbnae=heroku_d00a5c1a96446ed',
+			'dsn'        => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_d00a5c1a96446ed',
 			'username'   => 'b7d02971d6333a',
 			'password'   => 'acb52f8f',
 		),
